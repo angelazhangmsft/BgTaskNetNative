04e663cd5f2400fdfa0771a517892da5ec5a2fa1
Angela Zhang <angzhang@microsoft.com>
2021-01-11 14:27:03 -0800

    Add project files.

---
+++ CoreAppCPPUWP/App.cpp
@@ -0,0 +1,179 @@
+#include "pch.h"
+#include <winrt/OldWRC_CSharp.h>
+#include "iostream"
+
+using namespace winrt;
+using namespace std;;
+using namespace Windows;
+using namespace Windows::ApplicationModel::Core;
+using namespace Windows::Foundation::Numerics;
+using namespace Windows::UI;
+using namespace Windows::UI::Core;
+using namespace Windows::UI::Composition;
+
+struct App : implements<App, IFrameworkViewSource, IFrameworkView>
+{
+    CompositionTarget m_target{ nullptr };
+    VisualCollection m_visuals{ nullptr };
+    Visual m_selected{ nullptr };
+    float2 m_offset{};
+
+    IFrameworkView CreateView()
+    {
+        return *this;
+    }
+
+    void Initialize(CoreApplicationView const &)
+    {
+    }
+
+    void Load(hstring const&)
+    {
+    }
+
+    void Uninitialize()
+    {
+    }
+
+    void Run()
+    {
+        CoreWindow window = CoreWindow::GetForCurrentThread();
+        window.Activate();
+
+        CoreDispatcher dispatcher = window.Dispatcher();
+        dispatcher.ProcessEvents(CoreProcessEventsOption::ProcessUntilQuit);
+    }
+
+    void SetWindow(CoreWindow const & window)
+    {
+        Compositor compositor;
+        ContainerVisual root = compositor.CreateContainerVisual();
+        m_target = compositor.CreateTargetForCurrentView();
+        m_target.Root(root);
+        m_visuals = root.Children();
+
+        window.PointerPressed({ this, &App::OnPointerPressed });
+        window.PointerMoved({ this, &App::OnPointerMoved });
+
+        window.PointerReleased([&](auto && ...)
+        {
+            m_selected = nullptr;
+        });
+    }
+
+    void OnPointerPressed(IInspectable const &, PointerEventArgs const & args)
+    {
+        //OldWRC_CSharp::BgTask task;
+        std::wstring exampleTaskName{ L"BgTask" };
+
+        auto allTasks{ Windows::ApplicationModel::Background::BackgroundTaskRegistration::AllTasks() };
+
+        bool taskRegistered{ false };
+        for (auto const& task : allTasks)
+        {
+            if (task.Value().Name() == exampleTaskName)
+            {
+                taskRegistered = true;
+                break;
+            }
+        }
+
+        if (!taskRegistered)
+        {
+            Windows::ApplicationModel::Background::BackgroundTaskBuilder builder;
+            builder.Name(exampleTaskName);
+            builder.TaskEntryPoint(L"OldWRC_Csharp.BgTask");
+            builder.SetTrigger(Windows::ApplicationModel::Background::SystemTrigger{
+                Windows::ApplicationModel::Background::SystemTriggerType::TimeZoneChange, false });
+            Windows::ApplicationModel::Background::BackgroundTaskRegistration task{ builder.Register() };
+        }
+
+        
+        float2 const point = args.CurrentPoint().Position();
+
+        for (Visual visual : m_visuals)
+        {
+            float3 const offset = visual.Offset();
+            float2 const size = visual.Size();
+
+            if (point.x >= offset.x &&
+                point.x < offset.x + size.x &&
+                point.y >= offset.y &&
+                point.y < offset.y + size.y)
+            {
+                m_selected = visual;
+                m_offset.x = offset.x - point.x;
+                m_offset.y = offset.y - point.y;
+            }
+        }
+
+        if (m_selected)
+        {
+            m_visuals.Remove(m_selected);
+            m_visuals.InsertAtTop(m_selected);
+        }
+        else
+        {
+            AddVisual(point);
+        }
+    }
+
+    void OnPointerMoved(IInspectable const &, PointerEventArgs const & args)
+    {
+        if (m_selected)
+        {
+            float2 const point = args.CurrentPoint().Position();
+
+            m_selected.Offset(
+            {
+                point.x + m_offset.x,
+                point.y + m_offset.y,
+                0.0f
+            });
+        }
+    }
+
+    void AddVisual(float2 const point)
+    {
+        Compositor compositor = m_visuals.Compositor();
+        SpriteVisual visual = compositor.CreateSpriteVisual();
+
+        static Color colors[] =
+        {
+            { 0xDC, 0x5B, 0x9B, 0xD5 },
+            { 0xDC, 0xED, 0x7D, 0x31 },
+            { 0xDC, 0x70, 0xAD, 0x47 },
+            { 0xDC, 0xFF, 0xC0, 0x00 }
+        };
+
+        static unsigned last = 0;
+        unsigned const next = ++last % _countof(colors);
+        visual.Brush(compositor.CreateColorBrush(colors[next]));
+
+        float const BlockSize = 100.0f;
+
+        visual.Size(
+        {
+            BlockSize,
+            BlockSize
+        });
+
+        visual.Offset(
+        {
+            point.x - BlockSize / 2.0f,
+            point.y - BlockSize / 2.0f,
+            0.0f,
+        });
+
+        m_visuals.InsertAtTop(visual);
+
+        m_selected = visual;
+        m_offset.x = -BlockSize / 2.0f;
+        m_offset.y = -BlockSize / 2.0f;
+    }
+};
+
+int __stdcall wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
+{
+    CoreApplication::Run(make<App>());
+}
---
+++ OldWRC_CSharp/BgTask.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.Background;
+using Windows.Data.Xml.Dom;
+using Windows.System.Threading;
+using Windows.UI.Notifications;
+
+namespace OldWRC_CSharp
+{
+    public sealed class BgTask
+    {
+        BackgroundTaskCancellationReason _cancelReason = BackgroundTaskCancellationReason.Abort;
+        volatile bool _cancelRequested = false;
+        BackgroundTaskDeferral _deferral = null;
+        ThreadPoolTimer _periodicTimer = null;
+        uint _progress = 0;
+        IBackgroundTaskInstance _taskInstance = null;
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            Debug.WriteLine("Background " + taskInstance.Task.Name + " Starting...");
+
+            // Query BackgroundWorkCost
+            var cost = BackgroundWorkCost.CurrentBackgroundWorkCost;
+            if (cost == BackgroundWorkCostValue.High) return;
+
+            // Associate a cancellation handler with the background task.
+            taskInstance.Canceled += new BackgroundTaskCanceledEventHandler(OnCanceled);
+
+
+            _taskInstance = taskInstance;
+            SendToast();
+
+            _periodicTimer = ThreadPoolTimer.CreatePeriodicTimer(new TimerElapsedHandler(PeriodicTimerCallback), TimeSpan.FromSeconds(1));
+
+        }
+
+        private void SendToast()
+        {
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastImageAndText01);
+            XmlNodeList textElements = toastXml.GetElementsByTagName("text");
+            textElements[0].AppendChild(toastXml.CreateTextNode("A toast example"));
+            textElements[1].AppendChild(toastXml.CreateTextNode("Here's a message from your background task"));
+            ToastNotification notification = new ToastNotification(toastXml);
+            notification.ExpirationTime = DateTime.Now.AddMinutes(5);
+            ToastNotificationManager.CreateToastNotifier().Show(notification);
+        }
+
+        //
+        // Handles background task cancellation.
+        //
+        private void OnCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            //
+            // Indicate that the background task is canceled.
+            //
+            _cancelRequested = true;
+            _cancelReason = reason;
+
+            Debug.WriteLine("Background " + sender.Task.Name + " Cancel Requested...");
+        }
+
+        //
+        // Simulate the background task activity.
+        //
+        private void PeriodicTimerCallback(ThreadPoolTimer timer)
+        {
+            if ((_cancelRequested == false) && (_progress < 100))
+            {
+                _progress += 10;
+                _taskInstance.Progress = _progress;
+            }
+            else
+            {
+                _periodicTimer.Cancel();
+
+                //
+                // Record that this background task ran.
+                //
+                String taskStatus = (_progress < 100) ? "Canceled with reason: " + _cancelReason.ToString() : "Completed";
+                Debug.WriteLine("Background " + _taskInstance.Task.Name + taskStatus);
+
+                //
+                // Indicate that the background task has completed.
+                //
+                _deferral.Complete();
+            }
+        }
+
+        public static void Start(BackgroundTaskRegistrationGroup sender, BackgroundActivatedEventArgs args)
+        {
+            Start(args.TaskInstance);
+        }
+
+        public static void Start(IBackgroundTaskInstance taskInstance)
+        {
+            // Use the taskInstance.Name and/or taskInstance.InstanceId to determine
+            // what background activity to perform. In this sample, all of our
+            // background activities are the same, so there is nothing to check.
+            var activity = new BgTask();
+            activity.Run(taskInstance);
+        }
+    }
+}
